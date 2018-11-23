9a7f8a596d876aea1b73bacb2567526e8bdd0bb3
Fabian Streitel <streitel@cqse.eu>
2018-11-23 14:50:12 +0100

    implement simple proxy test program

--- ProxyTester/Program.cs
+++ ProxyTester/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProxyTester
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            string url = "http://google.com";
+            if (args.Length > 0)
+            {
+                url = args[0];
+            }
+
+            var proxy = WebRequest.DefaultWebProxy;
+            Console.WriteLine("Used Proxy: " + proxy?.GetProxy(new Uri(url))?.ToString() ?? "null");
+            Console.WriteLine("-----");
+
+            FetchAsync(url).Wait();
+        }
+
+        private static async Task FetchAsync(string url)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync(url);
+                string body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Connected successfully\nStatus: {response.StatusCode}\n\nResponse: {body.Substring(0, 500)}...");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed to connect\nException: {e.GetType()} - {e.Message}\n{e}");
+            }
         }
     }
 }
